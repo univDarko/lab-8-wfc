a10625817e6169154353261dc59b30af2e069e9b
Alejandro Gutierrez <alejandroxchile@gmail.com>
2025-10-27 15:25:14 -0300

    WFC terminado

--- Assets/Scripts/CustomTile.cs
+++ Assets/Scripts/CustomTile.cs
@@ -1,11 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Tilemaps;
 
 public class CustomTile : MonoBehaviour
 {
-    public Tile tile;
     public Vector4 directionType;
-    public int entropy = 7;
 }
--- Assets/Scripts/TileSpawner.cs
+++ Assets/Scripts/TileSpawner.cs
@@ -1,132 +1,198 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
-using UnityEngine.Tilemaps;
-using static UnityEditor.Progress;
 
 public class TileSpawner : MonoBehaviour
 {
-    [SerializeField] private int size = 5;
+    [SerializeField] private int size = 10;
+    [SerializeField] private List<GameObject> tilePrefabs = new List<GameObject>();
 
-    [SerializeField] private List<GameObject> tiles = new List<GameObject>();
+    private class Cell
+    {
+        public bool isCollapsed = false;
+        public List<GameObject> possibleOptions;
+        public Vector2Int position;
 
-    // Start is called before the first frame update
+        public int Entropy => possibleOptions.Count;
+
+        public Cell(List<GameObject> allTilePrefabs, Vector2Int pos)
+        {
+            possibleOptions = new List<GameObject>(allTilePrefabs);
+            position = pos;
+        }
+    }
+
+    private Cell[,] grid;
+    private List<Cell> uncollapsedCells = new List<Cell>();
+
     void Start()
     {
-        WaveFunctionCollapse();
-        //SpawnRandom();
+        StartGeneration();
     }
 
-    private void SpawnRandom()
+    void Update()
     {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            StopAllCoroutines();
+            ClearGrid();
+            StartGeneration();
+        }
+    }
+
+    void StartGeneration()
+    {
+        uncollapsedCells.Clear();
+        InitializeGrid();
+        StartCoroutine(RunWFC());
+    }
+
+    void ClearGrid()
+    {
+        foreach (Transform child in transform)
+        {
+            Destroy(child.gameObject);
+        }
+    }
+
+    void InitializeGrid()
+    {
+        grid = new Cell[size, size];
+
         for (int i = 0; i < size; i++)
         {
             for (int j = 0; j < size; j++)
             {
-                Instantiate(tiles[Random.Range(0, tiles.Count)], new Vector2(i, j), Quaternion.identity);
+                Cell newCell = new Cell(tilePrefabs, new Vector2Int(i, j));
+                grid[i, j] = newCell;
+                uncollapsedCells.Add(newCell);
             }
         }
-
-        
-        
     }
 
-    private bool CheckComp(Vector4 a, Vector4 b)
+    private IEnumerator RunWFC()
     {
-        if ((a.y == b.w) ||
-            (a.x == b.z) ||
-            (a.w == b.y) ||
-            (a.z == b.x))
+        while (uncollapsedCells.Count > 0)
         {
-            return true;
-        }
+            Cell cellToCollapse = GetCellWithLowestEntropy();
 
-        return false;
-    }
-
-    private void CheckAdjacent(List<GameObject> maplist, GameObject current)
-    {
-        int aux = 0;
-        for (int i = 0; i < size; i++)
-        {
-            for (int j = 0; j < size; j++)
+            if (cellToCollapse != null)
             {
-                if (current.transform.position.x == i &&
-                    current.transform.position.y == j)
+                CollapseCell(cellToCollapse);
+
+                Propagate(cellToCollapse.position);
+            }
+            else
+            {
+                Debug.LogError("WFC fallo");
+                break;
+            }
+            yield return new WaitForSeconds(0.1f);
+        }
+        yield return null;
+    }
+
+    private Cell GetCellWithLowestEntropy()
+    {
+        if (uncollapsedCells.Count == 0) return null;
+
+        //Ordena la lista por entropía
+        uncollapsedCells = uncollapsedCells.OrderBy(c => c.Entropy).ToList();
+
+        //Filtra las celdas con la entropía más baja (pueden ser varias)
+        int lowestEntropy = uncollapsedCells[0].Entropy;
+        List<Cell> cellsWithLowestEntropy = uncollapsedCells.Where(c => c.Entropy == lowestEntropy).ToList();
+
+        //Elege una al azar entre las de menor entropía
+        return cellsWithLowestEntropy[Random.Range(0, cellsWithLowestEntropy.Count)];
+    }
+
+    private void CollapseCell(Cell cell)
+    {
+        //Elegir un prefab aleatorio de las opciones restantes
+        GameObject chosenPrefab = cell.possibleOptions[Random.Range(0, cell.Entropy)];
+
+        cell.possibleOptions = new List<GameObject> { chosenPrefab };
+        cell.isCollapsed = true;
+        uncollapsedCells.Remove(cell);
+
+        Instantiate(chosenPrefab, new Vector2(cell.position.x, cell.position.y), Quaternion.identity, this.transform);
+    }
+
+    private void Propagate(Vector2Int collapsedPos)
+    {
+        Stack<Vector2Int> stack = new Stack<Vector2Int>();
+        stack.Push(collapsedPos);
+
+        while (stack.Count > 0)
+        {
+            Vector2Int currentPos = stack.Pop();
+            Cell currentCell = grid[currentPos.x, currentPos.y];
+
+            Vector2Int[] neighborPositions = new Vector2Int[]
+            {
+                currentPos + Vector2Int.up,    // Arriba
+                currentPos + Vector2Int.down,  // Abajo
+                currentPos + Vector2Int.left,  // Izquierda
+                currentPos + Vector2Int.right  // Derecha
+                  };
+
+            foreach (Vector2Int neighborPos in neighborPositions)
                 {
-                    foreach (GameObject t in tiles)
+                if (neighborPos.x < 0 || neighborPos.x >= size || neighborPos.y < 0 || neighborPos.y >= size)
+                    continue;
+
+                Cell neighborCell = grid[neighborPos.x, neighborPos.y];
+
+                //si el vecino ya está colapsado, no hace nadaxd
+                if (neighborCell.isCollapsed) continue;
+
+                int initialEntropy = neighborCell.Entropy;
+                List<GameObject> validOptions = new List<GameObject>(neighborCell.possibleOptions);
+
+                List<float> validSockets = GetValidSockets(currentCell, neighborPos - currentPos);
+
+                //Elimina opciones no válidas del vecino
+                validOptions.RemoveAll(optionPrefab =>
+                {
+                    CustomTile optionTile = optionPrefab.GetComponent<CustomTile>();
+                    float socketToMatch = GetSocketForDirection(optionTile, currentPos - neighborPos);
+                    return !validSockets.Contains(socketToMatch);
+                });
+
+                if (validOptions.Count < initialEntropy)
+                {
+                    neighborCell.possibleOptions = validOptions;
+
+                    if (!stack.Contains(neighborPos))
                     {
-                        if (CheckComp(current.GetComponent<CustomTile>().directionType,
-                            t.GetComponent<CustomTile>().directionType))
-                        {
-                            aux++;
-                        }
+                        stack.Push(neighborPos);
                     }
-                    current.GetComponent<CustomTile>().entropy = aux;
-                    break;
                 }
             }
         }
     }
-
-    private void WaveFunctionCollapse()
+    private List<float> GetValidSockets(Cell cell, Vector2Int direction)
     {
-        List<GameObject> objList = new List<GameObject>();
-        int count = size * size;
-
-        for (int i = 0; i < size; i++)
+        List<float> sockets = new List<float>();
+        foreach (GameObject prefab in cell.possibleOptions)
         {
-            for (int j = 0; j < size; j++)
+            float socket = GetSocketForDirection(prefab.GetComponent<CustomTile>(), direction);
+            if (!sockets.Contains(socket))
             {
-                GameObject t = Instantiate(tiles[Random.Range(0, tiles.Count)],
-                    new Vector2(i, j), Quaternion.identity);
-
-                objList.Add(t);
+                sockets.Add(socket);
             }
-            
         }
-
-        while (count > 0) 
-        {
-            GameObject temp;
-
-            if (count == size * size)
-            {
-                objList.Add(Instantiate(tiles[Random.Range(0, tiles.Count)],
-                    new Vector2(Random.Range(0, size), Random.Range(0, size)), Quaternion.identity));
-                count--;
-                continue;
-            }
-
-            int smallest = 999;
-
-            for (int i = 0; i < size*size; i++)
-            {
-                if (objList[i].GetComponent<CustomTile>().entropy < smallest)
-                {
-                    smallest = objList[i].GetComponent<CustomTile>().entropy;
-                }
-            }
-
-            Vector3 pos = new Vector2(Random.Range(0, size), Random.Range(0, size));
-
-            for (int i = 0; i < objList.Count; i++)
-            {
-                if (objList[i].GetComponent<CustomTile>().entropy == smallest)
-                {
-                    pos = objList[i].transform.position;
-                }
-            }
-
-            temp = Instantiate(tiles[Random.Range(0, tiles.Count)],
-                    pos, Quaternion.identity);
-
-            CheckAdjacent(objList, temp);
-
-            objList.Add(temp);
-
-            count--;
-        }
-
+        return sockets;
     }
+
+    private float GetSocketForDirection(CustomTile tile, Vector2Int direction)
+    {
+        if (direction == Vector2Int.up) return tile.directionType.x;      // Arriba (x)
+        if (direction == Vector2Int.right) return tile.directionType.y;    // Derecha (y)
+        if (direction == Vector2Int.down) return tile.directionType.z;     // Abajo (z)
+        if (direction == Vector2Int.left) return tile.directionType.w;     // Izquierda (w)
+        return -999;//Error
+    }
 }
